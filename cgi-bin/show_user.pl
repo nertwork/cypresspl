10df4f252fbd28bec9d78271edb64006a9b0f245
nertwork <webmaster@nertwork.com>
2015-10-31 01:55:32 -0700

    chaning more show_user stuff

--- cgi-bin/show_user.pl
+++ cgi-bin/show_user.pl
@@ -5,9 +5,8 @@ use strict;
 
 print "Content-Type: text/html\n\n";
 
-my ($remote_hostname, $ENV);
 if (exists$ENV{'REMOTE_ADDR'}) {
-	$remote_hostname = gethostbyaddr(inet_aton($ENV{'REMOTE_ADDR'}), AF_INET); 
+	my $remote_hostname = gethostbyaddr(inet_aton($ENV{'REMOTE_ADDR'}), AF_INET); 
 	print "Remote Hostname: ", $remote_hostname, "<br>\n";
 	print "Remote IP: ", $ENV{'REMOTE_ADDR'}, "<br>\n";
 	print "User Agent: ", $ENV{'HTTP_USER_AGENT'}, "<br>\n";
