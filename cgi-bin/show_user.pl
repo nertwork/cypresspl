50c0bc6ae92c25a55ee6da5ea71101c2ddd4ce86
nertwork <webmaster@nertwork.com>
2015-10-31 01:30:57 -0700

    fixing strict show_user

--- cgi-bin/show_user.pl
+++ cgi-bin/show_user.pl
@@ -1,16 +1,17 @@
 #!/usr/bin/perl
 
 use Socket;
-use strict;
-use CGI qw(:standard);
-
-print start_html;
-my ($remote_ip, $remote_hostname);
-
-$remote_hostname = gethostbyaddr(inet_aton($ENV{'REMOTE_ADDR'}), AF_INET); 
+use warnings;
 
 print "Content-Type: text/html\n\n";
-print "Remote Hostname: ", $remote_hostname, "<br>\n";
-print "Remote IP: ", $ENV{'REMOTE_ADDR'}, "<br>\n";
-print "User Agent: ", $ENV{'HTTP_USER_AGENT'}, "<br>\n";
-print end_html;
+
+my ($remote_hostname, $ENV);
+if (exists$ENV{'REMOTE_ADDR'}) {
+	$remote_hostname = gethostbyaddr(inet_aton($ENV{'REMOTE_ADDR'}), AF_INET); 
+	print "Remote Hostname: ", $remote_hostname, "<br>\n";
+	print "Remote IP: ", $ENV{'REMOTE_ADDR'}, "<br>\n";
+	print "User Agent: ", $ENV{'HTTP_USER_AGENT'}, "<br>\n";
+} else {
+	print "No IP is used";
+}
+
