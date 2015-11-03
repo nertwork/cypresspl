9e4c27f3c834e70c1f23ec70d06a92bb11cedb4f
nertwork <webmaster@nertwork.com>
2015-11-02 17:44:46 -0800

    exam1-1

---
+++ labs/exam1_1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+## File name: exam1_1.pl
+## Student: James Farr
+use CGI qw(:standard);
+print header, start_html;
+print start_form(-action=>$ENV{SCRIPT_NAME}, -method=>'post');
+print "<p>How much do you weigh?: <INPUT type='text' NAME='weight' SIZE=4></p>";
+print p, submit(-name =>'Send');
+print end_form;
+if (param('weight')) {
+	$weight = param('weight') * 0.453592;
+	print "You selected ", $weight, "!<br>";
+} else { }
+print end_html;
+
