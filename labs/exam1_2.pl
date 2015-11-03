cf73f69a86db8fa8014dc29da7d37305ec708a9d
nertwork <webmaster@nertwork.com>
2015-11-02 17:58:02 -0800

    adding in exam 1_2

---
+++ labs/exam1_2.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+## File name: exam1_2.pl
+## Student: James Farr
+use CGI qw(:standard);
+print header, start_html;
+($school, $unit, $cost) = ("Cypress College", 3, 17.5);
+$ammount = $unit * $cost;
+print "$unit units at $school at $cost each is \$$ammount";
+print end_html;
