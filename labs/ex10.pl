c437cb9f3530ab4d75a8fab1c891a2a3d368bc68
nertwork <webmaster@nertwork.com>
2015-11-24 17:03:00 -0800

    fixing ex10

--- labs/ex10.pl
+++ labs/ex10.pl
@@ -5,15 +5,16 @@ use strict;
 use CGI qw(:standard);
 print header, start_html;
 if (!param('sentence'))
-{ 
-	print "<form action='$ENV{'SCRIPT_NAME'}' method='post'>\n";
-	print "Enter a sentence: <input type='text' name='sentence' cols='60'>\n";
-	print "<input type='Submit'>";
-	print "</form>";
+{
+        print "<form action='$ENV{'SCRIPT_NAME'}' method='post'>\n";
+        print "Enter a sentence: ";
+        print "<textarea name='sentence' rows='1' cols='60'></textarea></br>\n";
+        print "<input type='Submit'>";
+        print "</form>";
 } else {
-	my $sentence = param('sentence');
-	$sentence =~ tr/a-z/A-Z/;
-	print $sentence;
+        my $sentence = param('sentence');
+        $sentence =~ tr/a-z/A-Z/;
+        print $sentence;
 }
 
 print end_html;
